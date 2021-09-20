23e07c008c7869c5df8e0a33caac9474abcf5395
Lester Cerioli <lesterlucasit@hotmail.com>
2021-09-20 01:22:58 +0000

    Implementando classes de Command

---
+++ ALOS/src/Domain/ALOS.Domain/Commands/Core/Address/RegisterNewAddressCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALOS.Domain.Commands.Core.Address
+{
+    public class RegisterNewAddressCommand
+    {
+    }
+}
