71b83b26bb00e2b5b35ea827125008c55dc4d0a5
Lester Cerioli <lesterlucasit@hotmail.com>
2021-09-20 00:16:39 +0000

    feat(#532): implementando interfaces de Dominio

---
+++ ALOS/src/Domain/ALOS.Domain/Interfaces/HR/ICPFRepository.cs
@@ -0,0 +1,22 @@
+﻿using ALOS.Domain.Models.HR;
+using NetDevPack.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALOS.Domain.Interfaces.HR
+{
+    public interface ICPFRepository : IRepository<CPF>
+    {
+        Task<CPF> GetById(Guid id);
+        Task<CPF> GetByCPFNumber(string cpfNumber);
+        Task<IEnumerable<CPF>> GetAll();
+
+        void Add(CPF cPF);
+        void Update(CPF cPF);
+        void Remove(CPF cPF);
+
+    }
+}
---
+++ ALOS/src/Domain/ALOS.Domain/Interfaces/ServiceDesk/ICategoriaRepository.cs
@@ -0,0 +1,15 @@
+﻿using ALOS.Domain.Models.ServiceDesk;
+using NetDevPack.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALOS.Domain.Interfaces.ServiceDesk
+{
+    public interface ICategoriaRepository : IRepository<Categoria>
+    {
+
+    }
+}
