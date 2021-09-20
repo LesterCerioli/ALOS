bd7e5c38d8af04fb3d20f430c9385340e176b12c
Lester Cerioli <lesterlucasit@hotmail.com>
2021-09-20 21:22:04 +0000

    Implementando classes de Command

---
+++ ALOS/src/Domain/ALOS.Domain/Commands/HR/Funcionario/RemoveFuncionarioCommand.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ALOS.Domain.Commands.HR.Funcionario
+{
+    public class RemoveFuncionarioCommand
+    {
+        
+    }
+}
---
+++ ALOS/src/Domain/ALOS.Domain/Commands/Servicer Desk/RegisterNewServiceDeskCommand.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ALOS.Domain.Commands.Servicer_Desk
+{
+    public class RegisterNewServiceDeskCommand
+    {
+        
+    }
+}
